d0d968153325a63cdcd035fe1cfb2e9c11933ff9
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-25 01:52:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,22 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "Hasta la vista, baby"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase_two = "It's Dangerous To Go Alone! Take This."
+  puts phrase_two
+end
+
+def all_phrases
+  mario
+  status
+  link
+end
+
+  
